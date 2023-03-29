41cccaef41d9992b7a39d62d2c16a4d845c58338
ltardivo <lauratardivo@gmail.com>
2023-03-29 13:45:33 -0300

    Adds new env vars to connect through user and password to the bitcoin node.

--- rpc.js
+++ rpc.js
@@ -1,6 +1,7 @@
 module.exports = require('yajrpc/qup')({
-  url: process.env.RPC || 'http://localhost:8332',
-  auth: require('fs').readFileSync(process.env.RPCCOOKIE),
+  url: process.env.RPC_URL || 'http://localhost:19332',
+  user: process.env.RPC_USER || 'bcn-admin',
+  pass: process.env.RPC_PASS || 'kH4nU5Okm6-uyC0_mA5ztVNacJqZbYd_KGLl6mx722A=',
   batch: process.env.RPCBATCHSIZE || 500,
   concurrent: process.env.RPCCONCURRENT || 16
 })
